e951560055e3771a1482f811b6fcc3887847fffa
Juliovsky <ybelova24@gmail.com>
2021-11-07 08:55:11 +0200

    node modules removed

--- node_modules/bootstrap-vue-3/src/directives/BModal.ts
+++
@@ -1,8 +0,0 @@
-import {DirectiveBinding} from 'vue'
-
-export default {
-  mounted(el: HTMLElement, binding: DirectiveBinding): void {
-    el.setAttribute('data-bs-toggle', 'modal')
-    el.setAttribute('data-bs-target', `#${binding.arg}`)
-  },
-}
